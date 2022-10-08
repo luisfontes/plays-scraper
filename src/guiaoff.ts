8e1b06f5e3c7224c57ca7c1a4657c6e83f550059
Luís Fontes <lfontes@hopper.com>
2022-10-08 12:03:30 -0300

    changes

--- src/guiaoff.ts
+++ src/guiaoff.ts
@@ -12,43 +12,39 @@ type Data = {
   run: string;
 };
 
-try {
-  const res = await fetch(url);
-  const html = await res.text();
-  const document = new DOMParser().parseFromString(html, 'text/html');
-  const list: Data[] = [];
+const res = await fetch(url);
+const html = await res.text();
+const document = new DOMParser().parseFromString(html, 'text/html');
+const list: Data[] = [];
 
-  const elems = document
-    ?.querySelector('.guia-categoria span[id="teatro"]')?.parentElement
-    ?.querySelectorAll('.guia-item');
+const elems = document
+  ?.querySelector('.guia-categoria span[id="teatro"]')?.parentElement
+  ?.querySelectorAll('.guia-item');
 
-  elems?.forEach((elem) => {
-    const el = elem as Element;
-    const title = el.querySelector('.guia-item-titulo')
-      ?.textContent || '';
-    const synopsis = el.querySelector('.guia-item-texto')?.textContent || '';
+elems?.forEach((elem) => {
+  const el = elem as Element;
+  const title = el.querySelector('.guia-item-titulo')
+    ?.textContent || '';
+  const synopsis = el.querySelector('.guia-item-texto')?.textContent || '';
 
-    const det1 = el.querySelectorAll('.guia-item-detalhe')[0] as Element;
-    const personnel = det1?.querySelector('p')?.textContent || '';
+  const det1 = el.querySelectorAll('.guia-item-detalhe')[0] as Element;
+  const personnel = det1?.querySelector('p')?.textContent || '';
 
-    const det2 = el.querySelectorAll('.guia-item-detalhe')[1] as Element;
-    const run = det2?.querySelector('p')?.textContent || '';
+  const det2 = el.querySelectorAll('.guia-item-detalhe')[1] as Element;
+  const run = det2?.querySelector('p')?.textContent || '';
 
-    if (synopsis && personnel && run) {
-      list.push({
-        title,
-        synopsis,
-        personnel,
-        run,
-      });
-    } else {
-      console.log(`Nao parseado: ${title}`);
-    }
-  });
-
-  if (list.length > 0) {
-    await Deno.writeTextFile(`./data/plays.json`, JSON.stringify(list));
+  if (synopsis && personnel && run) {
+    list.push({
+      title,
+      synopsis,
+      personnel,
+      run,
+    });
+  } else {
+    console.log(`Nao parseado: ${title}`);
   }
-} catch (error) {
-  console.log(error);
+});
+
+if (list.length > 0) {
+  await Deno.writeTextFile(`./data/plays.json`, JSON.stringify(list));
 }
